3076d4fbecbfa3b772677dffffc72c39846c0144
1530113 <Yabing.Li@sc.com>
2020-04-10 11:44:54 +0800

    增加组件toggle

---
+++ src/components/miui/toggle.vue
@@ -0,0 +1,63 @@
+<style lang="scss">
+    @import "~@miui/component-style/toggle";
+</style>
+
+<template>
+    <label
+        class="miui-toggle-wrapper"
+        :title="title"
+    >
+        <span
+            @click="swtichToggle"
+            @transitionstart="isAnimationPlaying = true"
+            @transitionend="isAnimationPlaying = false"
+            :class="{
+                'miui-toggle': true,
+                'miui-toggle_disabled': disabled,
+                'miui-toggle_checked': checked
+            }"
+        >
+        </span>
+        <span
+            v-if="label"
+            :class="{
+                'label': true,
+                'label_checked': checked,
+                [labelClass]: true
+            }"
+        >
+            {{label}}
+        </span>
+    </label>
+</template>
+
+<script>
+export default {
+    name: 'MIUI-Toggle',
+    model: {
+        prop: 'value',
+        event: 'change'
+    },
+    props: {
+        disabled: Boolean,
+        value: Boolean,
+        label: String,
+        labelClass: String,
+        title: String
+    },
+    data() {
+        return {
+            checked: this.$props.value,
+            isAnimationPlaying: false
+        }
+    },
+    methods: {
+        swtichToggle(e) {
+            if (this.$props.disabled || this.isAnimationPlaying) return
+            let value = !this.checked
+            this.$emit('change', value)
+            this.checked = value
+        }
+    }
+}
+</script>
