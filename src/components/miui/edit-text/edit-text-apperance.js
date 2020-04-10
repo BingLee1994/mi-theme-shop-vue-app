af438ffb79f620224ba0214c1fab2af6aed78b21
1530113 <Yabing.Li@sc.com>
2020-04-10 11:45:08 +0800

    增加组件edittext

---
+++ src/components/miui/edit-text/edit-text-apperance.js
@@ -0,0 +1,133 @@
+
+import { isEmpty, warn } from '../utils'
+
+const validator = document.createElement('div')
+
+function isValidStyle(name, val) {
+    let isValid = false
+    validator.setAttribute('style', '')
+    validator.style[name] = val
+    isValid = !isEmpty(validator.style[name])
+    return isValid
+}
+
+export default class EditTextAppearance {
+    constructor(bgColor, color, bgImage, caretColor, borderColor, borderWidth, borderRadius = '') {
+        let style = {}
+        style.backgroundColor = isValidStyle('backgroundColor', bgColor) ? bgColor : ''
+        style.color = isValidStyle('color', color) ? color : ''
+        style.backgroundImage = isValidStyle('backgroundImage', bgImage) ? bgImage : ''
+        style.caretColor = isValidStyle('caretColor', caretColor) ? caretColor : ''
+        style.borderRadius = isValidStyle('borderRadius', borderRadius) ? borderRadius : ''
+        this.__style__ = style
+        this.__borderColor__ = isValidStyle('borderColor', borderColor) ? borderColor : 'var(--accentColor)'
+        this.__borderWidth__ = isValidStyle('borderWidth', borderWidth) ? borderWidth : '2px'
+        this._setBoxShadow()
+    }
+
+    _setBoxShadow() {
+        let boxShadow = ''
+        if (!isEmpty(this.__borderColor__) && !isEmpty(this.__borderWidth__)) {
+            boxShadow = `0 0 0 ${this.__borderWidth__} inset ${this.__borderColor__}`
+        }
+        this._checkAndSetStyle('boxShadow', boxShadow)
+    }
+
+    _checkAndSetStyle(key, val) {
+        if (!isValidStyle(key, val)) {
+            warn(`Invalid style value '${val}' for '${key}.`)
+            val = ''
+        }
+        this.__style__[key] = val
+    }
+
+    get backgroundColor() {
+        return this.__style__.backgroundColor
+    }
+
+    set backgroundColor(val) {
+        this._checkAndSetStyle('backgroundColor', val)
+    }
+
+    get color() {
+        return this.__style__.color
+    }
+
+    set color(val) {
+        this._checkAndSetStyle('color', val)
+    }
+
+    get backgroundImage() {
+        return this.__style__.backgroundImage
+    }
+
+    set backgroundImage(val) {
+        this._checkAndSetStyle('backgroundImage', val)
+    }
+
+    get caretColor() {
+        return this.__style__.caretColor
+    }
+
+    set caretColor(val) {
+        this._checkAndSetStyle('caretColor', val)
+    }
+
+    get borderColor() {
+        return this.__borderColor__
+    }
+
+    set borderColor(val) {
+        if (!isValidStyle('borderColor', val)) {
+            warn(`Invalid style value '${val}' for 'borderColor'.`)
+            val = ''
+        }
+        this.__borderColor__ = val
+        this._setBoxShadow()
+    }
+
+    get borderWidth() {
+        return this.__borderWidth__
+    }
+
+    set borderWidth(val) {
+        if (!isValidStyle('borderWidth', val)) {
+            warn(`Invalid style value '${val}' for 'borderWidth'.`)
+            val = ''
+        }
+        this.__borderWidth__ = val
+        this._setBoxShadow()
+    }
+
+    get borderRadius() {
+        return this.__style__.borderRadius
+    }
+
+    set borderRadius(val) {
+        this._checkAndSetStyle('borderRadius', val)
+    }
+
+    toStyle() {
+        let style = Object.create(null)
+        for (let name in this.__style__) {
+            let val = this.__style__[name]
+            if (!isEmpty(val)) {
+                style[name] = val
+            }
+        }
+        return style
+    }
+
+    toStyleString() {
+        let styleStr = ''
+        validator.setAttribute('style', '')
+        for (let name in this.__style__) {
+            let val = this.__style__[name]
+            if (!isEmpty(val)) {
+                validator.style[name] = val
+            }
+        }
+        styleStr = validator.getAttribute('style')
+        return styleStr
+    }
+}
---
+++ src/components/miui/edit-text/edit-text.vue
@@ -0,0 +1,182 @@
+<style lang='scss'>
+@import "~@miui/component-style/edit-text";
+</style>
+
+<script>
+import { warn, joinClass, isEmpty, isNone } from '../utils'
+import EditTextAppearance from './edit-text-apperance'
+
+const expectedType = ['text', 'number', 'password']
+const expectedSlotTag = ['span', 'font']
+
+export default {
+    name: 'MIUI-EditText',
+    model: {
+        prop: 'value',
+        event: 'change'
+    },
+    props: {
+        widen: Boolean,
+        disabled: Boolean,
+        value: [String, Number],
+        prefixIconClass: String,
+        suffixIconClass: String,
+        placeholder: String,
+        width: [Number, String],
+        showPasswordButton: Boolean,
+        showClearButton: Boolean,
+        name: String,
+        appearance: {
+            type: EditTextAppearance,
+            default: null
+        },
+        type: {
+            validator(value) {
+                return expectedType.indexOf(value) !== -1
+            }
+        }
+    },
+    data() {
+        return {
+            visualType: this.$props.type,
+            visualValue: this.$props.value,
+            isComposing: false
+        }
+    },
+    computed: {
+        showPrependText() {
+            return this._checkTextSlot('prepend-text')
+        },
+        showAppendText() {
+            return this._checkTextSlot('append-text')
+        }
+    },
+    watch: {
+        value(val) {
+            requestAnimationFrame(() => {
+                this._updateNativeInputValue(val)
+            })
+        }
+    },
+    methods: {
+        _checkTextSlot(slotName) {
+            let slot = this.$slots[slotName]
+            if (isEmpty(slot)) return false
+            let child = slot[0]
+            if (expectedSlotTag.some(t => t === child.tag) || child.tag === undefined) {
+                if (slot.length > 1) {
+                    warn(`Expected one child for slot [${slotName}], but got ${slot.length}`, this)
+                }
+                return true
+            }
+            warn(`Expected text, <${expectedSlotTag.join('>, <')}> for slot [${slotName}], but got <${(child.componentOptions && child.componentOptions.tag) || child.tag}>`, this)
+            return false
+        },
+        toggleShowPassword(e) {
+            if (this.type !== 'password') return
+            this.visualType = this.visualType === 'text' ? 'password' : 'text'
+        },
+        emitEvent(type, extra) {
+            return e => {
+                this.$emit(type, e, extra)
+            }
+        },
+        onCompositionStart(e) {
+            this.isComposing = true
+        },
+        onCompositionEnd(e) {
+            this.isComposing = false
+            this.onInput(e)
+        },
+        onInput(e) {
+            if (this.isComposing) return
+            this.visualValue = e.target.value
+            this.$emit('change', e.target.value, e)
+        },
+        clearInput() {
+            if (this.$refs.input) {
+                this._updateNativeInputValue()
+                this.$emit('change', '')
+            }
+        },
+        _updateNativeInputValue(val) {
+            val = isNone(val) ? '' : val
+            this.$refs.input.value = val
+            this.visualValue = val
+        }
+    },
+    mounted() {
+        this._updateNativeInputValue(this.value)
+    },
+    render() {
+        let {
+            widen, disabled, width,
+            prefixIconClass, name,
+            suffixIconClass, placeholder, type,
+            showPasswordButton, showClearButton, appearance
+        } = this.$props
+
+        let { visualType, showPrependText, showAppendText } = this
+
+        let style = { width: `${parseInt(width)}px` }
+        if (!isNone(appearance)) {
+            Object.assign(style, appearance.toStyle())
+        }
+
+        return (
+            <div
+                style={style}
+                class={
+                    joinClass(
+                        'miui-edit-text-wrapper',
+                        widen ? 'miui-edit-text-wrapper_widen' : '',
+                        disabled ? 'miui-edit-text-wrapper_disabled' : ''
+                    )
+                }
+            >
+                {/* if */ showPrependText &&
+                    <span class="prepend-text">{this.$slots['prepend-text'][0]}</span>
+                }
+
+                {/* if */ prefixIconClass &&
+                    <span class={joinClass('icon prefix-icon', prefixIconClass)}></span>
+                }
+
+                <input
+                    ref="input"
+                    name={name}
+                    placeholder={placeholder}
+                    type={visualType}
+                    disabled={disabled}
+                    onInput={this.onInput}
+                    onFocus={this.emitEvent('focus')}
+                    onBlur={this.emitEvent('blur')}
+                    onKeydown={this.emitEvent('keydown')}
+                    onKeyup={this.emitEvent('keyup')}
+                    onKeypress={this.emitEvent('keypress')}
+                    onCompositionstart={this.onCompositionStart}
+                    onCompositionend={this.onCompositionEnd}
+                    />
+
+                {/* if */ showAppendText &&
+                    <span class="append-text">{this.$slots['append-text'][0]}</span>
+                }
+
+                {/* if */ suffixIconClass &&
+                    <span class={joinClass('icon suffix-icon', suffixIconClass)}></span>
+                }
+
+                {/* if */ (type === 'password' && showPasswordButton) &&
+                    <button class='icon password-icon' onClick={this.toggleShowPassword}></button>
+                }
+
+                {/* if */ (showClearButton && !isEmpty(this.visualValue)) &&
+                    <button class='icon clear-icon' onClick={this.clearInput}></button>
+                }
+            </div>
+        )
+    }
+}
+
+export { EditTextAppearance }
+</script>
