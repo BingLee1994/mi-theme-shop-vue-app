f1da1b31845d366ff1f721c9d730fd84c99dbbb6
1530113 <Yabing.Li@sc.com>
2020-04-11 09:51:53 +0800

    新增组件action-bar

---
+++ src/components/miui/action-bar/action-bar.vue
@@ -0,0 +1,46 @@
+<style lang="scss">
+    @import "~@miui/component-style/action-bar/action-bar";
+</style>
+
+<template>
+    <div class="miui-action-bar-wrapper" :id="$attrs.id">
+        <div class="miui-action-bar">
+            <span v-if="showBackButton" class="action-button back-button" @click="onClickBackButton"></span>
+            <p v-if="title" class="title bold">{{title}}</p>
+            <span class="action-button right-button"></span>
+            <SearchEditText v-if="showNormalSearch" :placeholder="searchBoxPlaceholder"/>
+        </div>
+        <div v-if="showSeparateSearch" class="search-box-wrapper">
+            <SearchEditText v-if="showSeparateSearch" :placeholder="searchBoxPlaceholder"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import SearchEditText from './search-edit-text'
+import { isEmpty } from '../utils'
+
+export default {
+    components: { SearchEditText },
+    inheritAttrs: false,
+    props: {
+        title: String,
+        showSearchBox: Boolean,
+        showBackButton: Boolean,
+        searchBoxPlaceholder: String
+    },
+    computed: {
+        showSeparateSearch() {
+            return this.$props.showSearchBox && !isEmpty(this.$props.title)
+        },
+        showNormalSearch() {
+            return this.$props.showSearchBox && isEmpty(this.$props.title)
+        }
+    },
+    methods: {
+        onClickBackButton(e) {
+            console.log('back')
+        }
+    }
+}
+</script>
---
+++ src/components/miui/action-bar/search-edit-text.vue
@@ -0,0 +1,27 @@
+<script>
+import EditTextAppearance from '../edit-text/edit-text-apperance'
+import EditText from '../edit-text/edit-text'
+
+const searchEditTextAppearance = new EditTextAppearance('var(--black10)')
+searchEditTextAppearance.borderWidth = '0px'
+searchEditTextAppearance.caretColor = 'var(--black80)'
+searchEditTextAppearance.borderRadius = '22px'
+
+export default {
+    inheritAttrs: false,
+    render() {
+        return (
+            <EditText
+                placeholder={this.$attrs.placeholder}
+                {...this.$listeners}
+                appearance={searchEditTextAppearance}
+                showClearButton={true}
+                prefixIconClass="search"
+                widen
+                style={{ margin: 0 }}
+            >
+            </EditText>
+        )
+    }
+}
+</script>
