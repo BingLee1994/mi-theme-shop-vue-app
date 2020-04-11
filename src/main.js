7dd92fdd954c89d937b9ebf274d0763c3987ce59
Li, Yabing <791188716@qq.com>
2020-04-11 18:51:48 +0800

    添加css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import '@assets/js/font-size'
 import './style/reset.scss'
+import './style/app.scss'
 import Dialog from '@miui/dialog/install'
 
 const elApp = document.getElementById('app')
