22554571ee3c075805b0c2d5a8891e628098fea7
1530113 <Yabing.Li@sc.com>
2020-04-12 19:11:36 +0800

    在Action bar中引入action menu组件

--- src/components/miui/action-bar/action-bar.vue
+++ src/components/miui/action-bar/action-bar.vue
@@ -7,7 +7,7 @@
         <div class="miui-action-bar">
             <span v-if="showBackButton" class="action-button back-button" @click="onClickBackButton"></span>
             <p v-if="title" class="title bold">{{title}}</p>
-            <span class="action-button right-button"></span>
+            <span class="action-button right-button" @click="popupActionMenu"></span>
             <SearchEditText v-if="showNormalSearch" :placeholder="searchBoxPlaceholder"/>
         </div>
         <div v-if="showSeparateSearch" class="search-box-wrapper">
@@ -19,10 +19,13 @@
 <script>
 import SearchEditText from './search-edit-text'
 import { isEmpty } from '../utils'
+// import ActionMenu from './action-menu/menu'
+import actionMenu from './action-menu'
 
 export default {
     components: { SearchEditText },
     inheritAttrs: false,
+    initPosition: 0,
     props: {
         title: String,
         showSearchBox: Boolean,
@@ -40,6 +43,19 @@ export default {
     methods: {
         onClickBackButton(e) {
             console.log('back')
+        },
+        popupActionMenu(e) {
+            console.log(e)
+            actionMenu.showMenu({
+                menuItems: [
+                    { text: 123 },
+                    { text: 456 }
+                ],
+                position: {
+                    x: e.clientX,
+                    y: e.clientY
+                }
+            })
         }
     }
 }
