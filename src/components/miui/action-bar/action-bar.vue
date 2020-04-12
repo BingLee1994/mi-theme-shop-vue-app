65a0a7c58c2e99d4cefc1587ccb3fec6dd2fb892
Li, Yabing <791188716@qq.com>
2020-04-12 21:09:40 +0800

    移除无用代码

--- src/components/miui/action-bar/action-bar.vue
+++ src/components/miui/action-bar/action-bar.vue
@@ -5,9 +5,32 @@
 <template>
     <div class="miui-action-bar-wrapper" :id="$attrs.id">
         <div class="miui-action-bar">
-            <span v-if="showBackButton" class="action-button back-button" @click="onClickBackButton"></span>
+            <span
+                v-if="showBackButton"
+                class="action-button back-button"
+                @click="onClickBackButton"
+            ></span>
+
+            <span
+                v-if="showLeftButton"
+                :class="['action-button left-button', leftButtonClass]"
+                @click="onClickLeftButton"
+            ></span>
+
             <p v-if="title" class="title bold">{{title}}</p>
-            <span class="action-button right-button" @click="popupActionMenu"></span>
+
+            <span
+                v-if="showRightButton"
+                :class="['action-button right-button', rightButtonClass]"
+                @click="onClickRightButton"
+            ></span>
+
+            <span
+                v-if="showActionMenu"
+                class="action-button menu-button"
+                @click="popupActionMenu"
+            ></span>
+
             <SearchEditText v-if="showNormalSearch" :placeholder="searchBoxPlaceholder"/>
         </div>
         <div v-if="showSeparateSearch" class="search-box-wrapper">
@@ -18,9 +41,9 @@
 
 <script>
 import SearchEditText from './search-edit-text'
-import { isEmpty } from '../utils'
+import { isEmpty, isNone } from '../utils'
 // import ActionMenu from './action-menu/menu'
-import actionMenu from './action-menu'
+import showActionMenu from './action-menu'
 
 export default {
     components: { SearchEditText },
@@ -30,7 +53,13 @@ export default {
         title: String,
         showSearchBox: Boolean,
         showBackButton: Boolean,
-        searchBoxPlaceholder: String
+        searchBoxPlaceholder: String,
+        showActionMenu: Boolean,
+        actionMenuItems: Array,
+        showRightButton: Boolean,
+        showLeftButton: Boolean,
+        rightButtonClass: String,
+        leftButtonClass: String
     },
     computed: {
         showSeparateSearch() {
@@ -42,20 +71,33 @@ export default {
     },
     methods: {
         onClickBackButton(e) {
-            console.log('back')
+            this.$emit('clickBackButton')
         },
-        popupActionMenu(e) {
-            console.log(e)
-            actionMenu.showMenu({
-                menuItems: [
-                    { text: 123 },
-                    { text: 456 }
-                ],
-                position: {
-                    x: e.clientX,
-                    y: e.clientY
-                }
-            })
+        onClickLeftButton(e) {
+            this.onClickCustomizedButton(e)
+        },
+        onClickRightButton(e) {
+            this.onClickCustomizedButton(e, false)
+        },
+        _onClickCustomizedButton(e, isLeft = true) {
+            this.$emit(isLeft ? 'clickLeftButton' : 'clickRightButton')
+        },
+        async popupActionMenu(e) {
+            let { actionMenuItems: menuItems } = this.$props
+            if (isNone(menuItems) || menuItems.length === 0) return
+
+            try {
+                let selectedItem = await showActionMenu({
+                    menuItems,
+                    position: {
+                        x: e.clientX,
+                        y: e.clientY
+                    }
+                })
+                this.$emit('selectActionMenuItem', selectedItem)
+            } catch (err) {
+                this.$emit('actionMenuDismissed')
+            }
         }
     }
 }
