8b41e568a7c0910489d782b27f935d84cacb276c
1530113 <Yabing.Li@sc.com>
2020-04-12 19:10:40 +0800

    增加action bar menu item组件

---
+++ src/components/miui/action-bar/action-menu/menu-item.vue
@@ -0,0 +1,40 @@
+<style lang="scss">
+    @import "~@miui/component-style/action-bar/action-menu/menu-item.scss";
+</style>
+
+<script>
+export default {
+    name: 'MIUI-ActionMenuItem',
+    props: {
+        text: [String, Number],
+        disabled: Boolean,
+        divider: Boolean,
+        iconClass: String
+    },
+
+    methods: {
+        onClick(e) {
+            if (this.$props.disabled) return
+            this.$emit('click', e)
+        }
+    },
+
+    render() {
+        let { text, disabled, divider, iconClass } = this.$props
+        return (
+            <div class={divider ? 'menu-item-divider' : 'menu-item-wrapper'}>
+                { /* if */ !divider &&
+                    <p
+                        onClick={this.onClick}
+                        class={`menu-item ${disabled ? 'menu-item_disabled' : ''}`}>
+                        { /* if */ iconClass &&
+                            <span class={`icon ${iconClass}`}></span>
+                        }
+                        <span class='text'>{text}</span>
+                    </p>
+                }
+            </div>
+        )
+    }
+}
+</script>
