1b8654675ad714ddeec423725bfdb3187ba045e0
1530113 <Yabing.Li@sc.com>
2020-04-16 10:57:09 +0800

    新增组件SwiperActivity

---
+++ src/components/app/swiper-activity/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="swiper-activity-wrapper" ref="wrapper">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'SwiperActivity',
+    created() {
+        this.$slots.default[0].componentOptions.propsData.position = { position: 1 }
+    },
+    mounted() {
+    },
+    updated() {
+    },
+    methods: {
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .swiper-activity-wrapper {
+        width: 100%;
+        height: 100%;
+        min-width: 200px;
+        min-height: 200px;
+        position: relative;
+        -webkit-overflow-scrolling: auto;
+        border: 1px solid black;
+        box-sizing: border-box;
+    }
+</style>
