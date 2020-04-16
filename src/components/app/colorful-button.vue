1ea6918bddcb38f5a492e5a8af7f7a684ed3e481
1530113 <Yabing.Li@sc.com>
2020-04-16 10:24:14 +0800

     完成多彩button，添加随机色彩，暗色模式

--- src/components/app/colorful-button.vue
+++ src/components/app/colorful-button.vue
@@ -1,23 +1,41 @@
 <template>
     <button
-        class="colorful-button"
+        :class="{
+            'colorful-button': true,
+            'dark-mode': darkmode
+        }"
         v-on="$listeners"
-        :style="{borderColor: colorVal}"
+        ref="button"
     >
         <slot></slot>
     </button>
 </template>
 
 <script>
+const defaultColor = ['#d1d3eb', '#a9a4f1', '#9fd0cf', '#e3c8ab', '#efb7e9', '#bbcaa1', '#ffafaf', '#a1c9a9']
+
 export default {
     name: 'ColorfulButton',
     props: {
-        color: String
+        darkmode: Boolean,
+        color: {
+            type: String,
+            default() {
+                return defaultColor[Math.floor(Math.random() * defaultColor.length + 1)]
+            }
+        }
     },
-    computed: {
-        colorVal() {
-            console.log(this.color)
-            return this.color
+    mounted() {
+        this._setColor()
+    },
+    updated() {
+        this._setColor()
+    },
+    methods: {
+        _setColor() {
+            if (this.$props.color) {
+                this.$refs.button.style.setProperty('--color', this.$props.color)
+            }
         }
     }
 }
@@ -25,16 +43,39 @@ export default {
 
 <style scoped lang="scss">
     .colorful-button {
+        --color: rgb(160, 208, 208);
         height: 3rem;
         line-height: 3rem;
         text-align: center;
         padding: 0 1rem;
         border-radius: 0.5rem;
         border: 1px solid black;
-        border-color: #A0D0D0;
+        border-color: var(--color);
         box-sizing: border-box;
-        background: #E8F6F6;
         color: rgba(0,0,0,.7);
         font-size: 1.4rem;
+        position: relative;
+        overflow: hidden;
+        margin: 3px;
+        &::before {
+            content: '';
+            display: inline-block;
+            position: absolute;
+            left: 0;
+            top: 0;
+            bottom: 0;
+            right: 0;
+            background: var(--color);
+            z-index: -1;
+            filter: brightness(1.3) saturate(40%);
+        }
+        &:active {
+            &::before {
+                filter: brightness(1.2) saturate(40%);
+            }
+        }
+    }
+    .dark-mode {
+        color: rgba(255,255,255,.9);
     }
 </style>
