ba5f3f44ec4dbbf273041f01daf37ee04e540e93
Li, Yabing <791188716@qq.com>
2020-04-24 14:34:41 +0800

    导入navigation和search的vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import preference from './preference'
+import navigation from './navigation'
+import search from './search'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    preference
+    preference,
+    navigation,
+    search
   }
 })
