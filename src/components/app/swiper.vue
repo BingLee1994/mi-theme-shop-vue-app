a3e1e507e621a7dff66ed16b8fe516fbb3e1cd70
Li, Yabing <791188716@qq.com>
2020-05-18 19:11:15 +0800

    修复swiper

--- src/components/app/swiper.vue
+++ src/components/app/swiper.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="swiper-wrapper" ref="wrapper">
+    <div
+        class="swiper-wrapper"
+        ref="wrapper"
+    >
         <div
             class="sub-wrapper"
             ref="subWrapper"
@@ -52,10 +55,15 @@ const MOVE_THRESHOLD = 20 / 100
 const DEFAULT_INTERVAL_DURATION = 3 * 1000
 const DEFAULT_MAX_INTERVAL_DURATION = 10 * 1000
 const BG_COLOR = ['#E8F6F6', '#EFF1FE', '#FFF5EC', '#FAFEF0', '#FFECFD']
+const VISIBILITY_THRESHOLD = 0.5
+const TYPE_DATA_CHANGE = 0
+const TYPE_WIDTH_CHANGE = 1
+const RESIZE_DEBOUNCE_TIMER = 200
 
 export default {
     name: 'Swiper',
     props: {
+        name: String,
         items: {
             type: Array,
             default: () => [],
@@ -76,33 +84,40 @@ export default {
     },
 
     data() {
-        let swiperItems = []
-        let length = this.items.length
-        for (let i = 0; i < length; i++) {
-            swiperItems.push(this.items[i])
-        }
-        let enableCircle = length >= 2
-        if (enableCircle) {
-            swiperItems.push(this.items[0])
-            swiperItems.unshift(this.items[length - 1])
-        }
-
         return {
             position: 0,
             width: 0,
             height: 0,
-            length: swiperItems.length,
-            index: enableCircle ? 1 : 0,
-            swiperItems,
-            enableCircle,
-            indicatorLength: this.items.length
+            length: this.$props.items.length,
+            index: 0
         }
     },
 
     computed: {
+        enableCircle() {
+            return this.$props.items.length >= 2
+        },
+
+        indicatorLength() {
+            return this.$props.items.length
+        },
+
+        swiperItems() {
+            let swiperItems = []
+            let length = this.$props.items.length
+            for (let i = 0; i < length; i++) {
+                swiperItems.push(this.items[i])
+            }
+            if (length >= 2) {
+                swiperItems.push(this.items[0])
+                swiperItems.unshift(this.items[length - 1])
+            }
+            return swiperItems
+        },
+
         isIndexAtCircleEdge() {
             return this.enableCircle && (
-                this.index === this.swiperItems.length - 1 ||
+                this.index === this.$props.items.length + 1 ||
                 this.index === 0
             )
         },
@@ -120,83 +135,172 @@ export default {
     watch: {
         actualIndex() {
             this.$emit('change', this.actualIndex)
+        },
+
+        items(newProp, oldProp) {
+            this.requireReorder = newProp.length !== oldProp.length
+        }
+    },
+
+    created() {
+        if (this.enableCircle) {
+            this.index = 1
+            this.length = this.swiperItems.length
         }
     },
 
     mounted() {
-        let { offsetWidth, offsetHeight } = this.$refs.wrapper
-        this.width = offsetWidth
-        this.height = offsetHeight
         this.animationPlaying = false
         this.autoPlayTimerId = null
 
-        if (!this.length || this.length < 2) {
-            return
-        }
+        this._setSize()
 
-        this.rightBoundary = (-1 * offsetWidth * (Math.max(this.length, 1) - 1))
+        this.$refs.wrapper.style.width = this.width + 'px'
         if (this.enableCircle) {
             this.position = -this.width
         }
+
         this._lazyLoadImgIfNeed()
-
-        let { wrapper } = this.$refs
-        let startX = 0
-        let startPosition = this.position
-        let delta = 0
-        let passive = { passive: false }
-
-        wrapper.addEventListener('touchstart', e => {
-            delta = 0
-            startX = e.touches[0].clientX
-            startPosition = this.position
-            this._stopAutoPlay()
-        }, passive)
-
-        wrapper.addEventListener('touchmove', e => {
-            if (this.animationPlaying) return
-
-            delta = e.touches[0].clientX - startX
-            let newPosition = startPosition + delta
-
-            if (newPosition <= 0 && newPosition >= this.rightBoundary) {
-                this.position = newPosition
-            } else {
-                delta = 0
-            }
-            e.stopPropagation()
-            e.preventDefault()
-        }, passive)
-
-        let stopTouch = e => {
-            if (delta === 0 || this.animationPlaying) return
-
-            let { index, width } = this
-            let direction = MOVE_RIGHT
-            let targetPosition = startPosition
-            let targetIndex = index
-
-            if (delta > 0) {
-                direction = MOVE_LEFT
-            }
-
-            if (Math.abs(delta) > this.width * MOVE_THRESHOLD) {
-                let next = direction === MOVE_RIGHT ? 1 : -1
-                targetIndex = (index + next)
-                targetPosition = -1 * targetIndex * width
-            }
-            this._swipeToWithAnimation(targetIndex, targetPosition).then(_ => {
-                this._startAutoPlay()
-            })
-        }
-
-        wrapper.addEventListener('touchend', stopTouch, passive)
-        wrapper.addEventListener('touchcancel', stopTouch, passive)
-        this._startAutoPlay()
+        this._enabledTouchListener()
+        this._startAutoPlayIfNeed()
         this._genRandomColorfulBg()
+        this._enableWrapperIntersectionObserver()
+
+        document.addEventListener('visibilitychange', e => {
+            this._handleComponentVisibility(document.visibilityState === 'visible')
+        })
+
+        window.addEventListener('resize', this._adjustSwiperUIonResize)
+    },
+
+    updated() {
+        if (this.requireReorder) {
+            this.length = this.swiperItems.length
+            this._adjustSwiperUI(TYPE_DATA_CHANGE)
+            this.requireReorder = false
+        }
     },
 
     methods: {
+        _setSize() {
+            this.$refs.wrapper.style.width = ''
+            let { offsetWidth, offsetHeight } = this.$refs.wrapper
+            this.width = offsetWidth
+            this.height = offsetHeight
+            this.rightBoundary = (-1 * offsetWidth * (Math.max(this.length, 1) - 1))
+            this.$refs.wrapper.style.width = this.width + 'px'
+        },
+
+        _adjustSwiperUIonResize() {
+            clearTimeout(this.resizeDebounceTimer)
+            this.resizeDebounceTimer = setTimeout(() => {
+                this._adjustSwiperUI(TYPE_WIDTH_CHANGE)
+            }, RESIZE_DEBOUNCE_TIMER)
+        },
+
+        async _adjustSwiperUI(changeType) {
+            this._stopAutoPlay()
+            switch (changeType) {
+                case TYPE_DATA_CHANGE:
+                    this.index = 0
+                    this.position = 0
+                    if (this.enableCircle) {
+                        this.index = 1
+                        this.position = -this.width
+                    }
+                    break
+                case TYPE_WIDTH_CHANGE: {
+                    if (this.animationPlaying) {
+                        this.$refs.subWrapper.style.transition = ''
+                    }
+                    this._setSize()
+                    this.position = -this.width * this.index
+                    break
+                }
+            }
+            this.rightBoundary = (-1 * this.width * (Math.max(this.length, 1) - 1))
+            this._lazyLoadImgIfNeed()
+            this._genRandomColorfulBg()
+            this._startAutoPlayIfNeed()
+        },
+
+        _enabledTouchListener() {
+            let { wrapper } = this.$refs
+            let startX = 0
+            let startPosition = this.position
+            let delta = 0
+            let passive = { passive: false }
+
+            wrapper.addEventListener('touchstart', e => {
+                delta = 0
+                startX = e.touches[0].clientX
+                startPosition = this.position
+                this._stopAutoPlay()
+            }, passive)
+
+            wrapper.addEventListener('touchmove', e => {
+                if (this.animationPlaying) return
+
+                delta = e.touches[0].clientX - startX
+                let newPosition = startPosition + delta
+
+                if (newPosition <= 0 && newPosition >= this.rightBoundary) {
+                    this.position = newPosition
+                } else {
+                    delta = 0
+                }
+                e.stopPropagation()
+                e.preventDefault()
+            }, passive)
+
+            let stopTouch = e => {
+                if (delta === 0 || this.animationPlaying) return
+
+                let { index, width } = this
+                let direction = MOVE_RIGHT
+                let targetPosition = startPosition
+                let targetIndex = index
+
+                if (delta > 0) {
+                    direction = MOVE_LEFT
+                }
+
+                if (Math.abs(delta) > this.width * MOVE_THRESHOLD) {
+                    let next = direction === MOVE_RIGHT ? 1 : -1
+                    targetIndex = (index + next)
+                    targetPosition = -1 * targetIndex * width
+                }
+                this._swipeToWithAnimation(targetIndex, targetPosition).then(_ => {
+                    this._startAutoPlayIfNeed()
+                })
+            }
+
+            wrapper.addEventListener('touchend', stopTouch, passive)
+            wrapper.addEventListener('touchcancel', stopTouch, passive)
+            this.touchListenerEnabled = true
+        },
+
+        _enableWrapperIntersectionObserver() {
+            let wrapper = this.$refs.wrapper
+            let option = {
+                threshold: VISIBILITY_THRESHOLD
+            }
+            let handleChange = e => {
+                let { intersectionRatio } = e[0]
+                this._handleComponentVisibility(intersectionRatio >= VISIBILITY_THRESHOLD)
+            }
+            this.observer = new IntersectionObserver(handleChange, option)
+            this.observer.observe(wrapper)
+        },
+
+        _handleComponentVisibility(visible) {
+            if (visible) {
+                this._startAutoPlayIfNeed()
+            } else {
+                this._stopAutoPlay()
+            }
+        },
+
         _genRandomColorfulBg() {
             let { subWrapper } = this.$refs
             let children = subWrapper.children
@@ -206,12 +310,15 @@ export default {
             })
         },
 
-        _startAutoPlay() {
-            this.autoPlayTimerId = setInterval(this._autoPlayTimer, this.duration)
+        _startAutoPlayIfNeed() {
+            if (!this.autoPlayTimerId && this.length >= 2) {
+                this.autoPlayTimerId = setInterval(this._autoPlayTimer.bind(this), this.duration)
+            }
         },
 
         _stopAutoPlay() {
             clearInterval(this.autoPlayTimerId)
+            this.autoPlayTimerId = null
         },
 
         _autoPlayTimer() {
@@ -237,6 +344,9 @@ export default {
 
         _playTransitionAnimation(targetIndex, targetPosition) {
             return new Promise(resolve => {
+                if (!this.$refs.subWrapper) {
+                    return this._stopAutoPlay()
+                }
                 this.$refs.subWrapper.style.transition = `transform ${ANI_DURATION}s ease`
                 this.animationPlaying = true
                 this.position = targetPosition
@@ -253,7 +363,8 @@ export default {
         },
 
         _lazyLoadImgIfNeed() {
-            let { index, swiperItems: items, $refs } = this
+            let { index, length, swiperItems: items, $refs } = this
+            if (length === 0) return
             index = Math.abs(index)
             let curImg = $refs[`img_${index}`][0]
             let currentBg = curImg.style.backgroundImage
@@ -275,6 +386,11 @@ export default {
 
     destroyed() {
         this._stopAutoPlay()
+        if (this.observer) {
+            this.observer.disconnect(this.$refs.wrapper)
+            this.observer = null
+        }
+        window.removeEventListener('resize', this._adjustSwiperUIonResize)
     }
 }
 </script>
@@ -282,7 +398,7 @@ export default {
 <style scoped lang="scss">
     .swiper-wrapper {
         min-width: 200px;
-        min-height: 200px;
+        min-height: 100px;
         position: relative;
         -webkit-overflow-scrolling: auto;
         box-sizing: border-box;
