28e46ec3eec99a8608488bbeba9d7d659e73431e
Li, Yabing <791188716@qq.com>
2020-05-18 19:22:11 +0800

    增加转场动画

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
-  <MiuiDarkMode id='app' class='miui'>
-    <router-view></router-view>
+  <MiuiDarkMode id='app' tagName="div">
+    <keep-alive>
+      <transition :name="transitionName">
+        <router-view v-if="$route.meta.keepAlive"></router-view>
+      </transition>
+    </keep-alive>
+
+    <transition :name="transitionName">
+      <router-view v-if="!$route.meta.keepAlive"></router-view>
+    </transition>
   </MiuiDarkMode>
 </template>
 
 <script>
-  import MiuiDarkMode from '@miui/dark-mode'
+import MiuiDarkMode from '@miui/dark-mode'
 
 export default {
     components: { MiuiDarkMode },
-    data() {
-        return {}
+    computed: {
+      transitionName() {
+        let to = this.$route
+        let { routeHistory } = this.$router
+        let last = routeHistory.get(-3) || {}
+        if (to.name === last.name) {
+          routeHistory.back(2)
+          return 'screen-enter-right'
+        } else {
+          return 'screen-enter-left'
+        }
+      }
     }
 }
 </script>
+
+<style lang="scss">
+  .home-selected {
+    color: pink;
+  }
+  .me-selected {
+    color: blue;
+  }
+  .category-selected {
+    color: green;
+  }
+</style>
