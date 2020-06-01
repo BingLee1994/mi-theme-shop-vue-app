6e4e963d61a6cd5606e0d904386d9cd557b84200
1530113 <Yabing.Li@sc.com>
2020-06-02 07:41:41 +0800

    添加image button组件

---
+++ src/components/app/image-button.vue
@@ -0,0 +1,61 @@
+<template>
+    <a
+        v-on="$listeners"
+        v-lazy:background="src"
+        :class="['image-button cover', hasChildren? 'medium': '']"
+    >
+        <span
+            :class="['title', darkMode? 'dark-mode': '']"
+            :style="{ color: color }"
+        >
+            <slot></slot>
+        </span>
+    </a>
+</template>
+
+<script>
+export default {
+    name: 'ImageButton',
+    props: {
+        src: String,
+        darkMode: {
+            type: Boolean,
+            default: false
+        },
+        color: {
+            type: String,
+            default: 'var(--white80)'
+        }
+    },
+
+    computed: {
+        hasChildren() {
+            console.log(this.$slots.default)
+            return this.$slots.default
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .image-button {
+        background-color: var(--miOrange);
+        width: calc(50% - 10px);
+        margin: 5px;
+        height: 60px;
+        border-radius: 6px;
+        box-shadow: 0 0 0 1px var(--black05);
+        box-sizing: border-box;
+
+        &.medium {
+            padding: 10px;
+            height: 80px;
+        }
+
+        .title {
+            &.dark-mode {
+                color: var(--black80);
+            }
+        }
+    }
+</style>
