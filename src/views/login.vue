cdf690b62df6b488255b6ab3ef479503cd8f30bd
1530113 <Yabing.Li@sc.com>
2020-06-21 12:52:29 +0800

    修改样式

--- src/views/login.vue
+++ src/views/login.vue
@@ -78,7 +78,7 @@ export default {
     .login-form {
         .input-item {
             position: relative;
-            border-bottom: 1px solid rgba(0,0,0,.1);
+            border-bottom: 1px solid var(--black10);
             padding:10px 0;
 
             input {
