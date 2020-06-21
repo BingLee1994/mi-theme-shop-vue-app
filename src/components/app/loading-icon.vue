dc2e2dc5ca38d1488a31dabd6fbb9d0f96dae288
1530113 <Yabing.Li@sc.com>
2020-06-21 12:47:03 +0800

    增加loading icon

---
+++ src/components/app/loading-icon.vue
@@ -0,0 +1,79 @@
+<style lang="scss">
+@keyframes rotate {
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
+.loader {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &-icon {
+        --width: 20px;
+        --color: var(--black80);
+        --border-width: 2px;
+        width: var(--width);
+        height: var(--width);
+        border: var(--border-width) solid var(--color);
+        box-sizing: border-box;
+        border-radius: calc(var(--width) / 2);
+        position: relative;
+        animation: rotate 1.5s linear;
+        animation-iteration-count: infinite;
+        display: inline-block;
+        margin: 0 10px;
+
+        &::before {
+            content: '';
+            display: block;
+            width: 6px;
+            height: 6px;
+            border-radius: 3px;
+            background: var(--color);
+            position: absolute;
+            transform-origin: 10px 10px;
+            left: 2px;
+            top: 2px;
+        }
+
+        &.small {
+            --width: 15px;
+            --color: var(--black80);
+            --border-width: 1px;
+            margin: 0 5px;
+
+            &-text {
+                font-size: 1.3rem;
+            }
+        }
+    }
+
+    &-text {
+        font-size: 1.4rem;
+        &.small {
+            font-size: 1.3rem;
+        }
+    }
+}
+</style>
+
+<template>
+    <div class="loader">
+        <span :class="['loader-icon', small? 'small': '']"></span>
+        <span :class="['loader-text', small? 'small': '']"><slot></slot></span>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'LoadingLoaderIcon',
+    props: {
+        small: Boolean
+    }
+}
+</script>
--- src/components/app/loading.vue
+++ src/components/app/loading.vue
@@ -1,13 +1,4 @@
 <style lang="scss">
-@keyframes rotate {
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(360deg);
-    }
-}
-
 .loading-layer {
     display: flex;
     justify-content: center;
@@ -15,44 +6,6 @@
     flex-direction: column;
     z-index: 99;
     background: var(--white);
-
-    .loader {
-        display: flex;
-        justify-content: center;
-
-        &-icon {
-            --width: 20px;
-            --color: var(--black80);
-            width: var(--width);
-            height: var(--width);
-            border: 2px solid var(--color);
-            box-sizing: border-box;
-            border-radius: calc(var(--width) / 2);
-            position: relative;
-            animation: rotate 1.5s linear;
-            animation-iteration-count: infinite;
-            margin-bottom: 10px;
-            display: inline-block;
-            margin: 0 5px;
-
-            &::before {
-                content: '';
-                display: block;
-                width: 6px;
-                height: 6px;
-                border-radius: 3px;
-                background: var(--color);
-                position: absolute;
-                transform-origin: 10px 10px;
-                left: 2px;
-                top: 2px;
-            }
-        }
-
-        &-text {
-            font-size: 1.5rem;
-        }
-    }
 }
 
 .retry-button {
@@ -62,11 +15,9 @@
 
 <template>
     <div v-full-screen class="loading-layer">
-        <div v-if="!showRetry" class="loader">
-            <span class="loader-icon">
-            </span>
-            <span class="loader-text">{{loadingMessage}}</span>
-        </div>
+        <LoadingLoaderIcon v-if="!showRetry">
+            {{loadingMessage}}
+        </LoadingLoaderIcon>
         <div v-else>
             <span>{{retryMessage}}</span>
             <button class="retry-button" @click="$emit('clickRetry')">{{retryButton}}</button>
@@ -75,9 +26,11 @@
 </template>
 
 <script>
+import LoadingLoaderIcon from './loading-icon'
 
 export default {
     name: 'LoadingLayer',
+    components: { LoadingLoaderIcon },
     props: {
         showRetry: {
             type: Boolean,
