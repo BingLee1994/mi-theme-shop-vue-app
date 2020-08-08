8288a50f3c2fe3568fa48d3970db68d10aca184d
Li, Yabing <791188716@qq.com>
2020-08-08 14:50:03 +0800

    根据不同尺寸动态调整rem

--- src/assets/js/font-size.js
+++ src/assets/js/font-size.js
@@ -1,5 +1,45 @@
 (function() {
+    let config = [
+        {
+            minWidth: 375,
+            size: 1
+        },
+        {
+            minWidth: 480,
+            size: 1.3
+        },
+        {
+            minWidth: 640,
+            size: 1.4
+        },
+        {
+            minWidth: 767,
+            size: 1.6
+        },
+        {
+            minWidth: 1024,
+            size: 1.8
+        }
+    ]
     let { documentElement: htmlDom } = document
     let htmlFontSize = getComputedStyle(htmlDom).fontSize
-    htmlDom.style.fontSize = (10 / parseFloat(htmlFontSize) * 100) + '%'
+
+    function setRem() {
+        let screenW = window.innerWidth
+        let normalSize = (10 / parseFloat(htmlFontSize) * 100)
+
+        for (let i = config.length - 1; i >= 0; i--) {
+            let { minWidth, size } = config[i]
+            if (screenW >= minWidth) {
+                htmlDom.style.fontSize = (normalSize * size) + '%'
+                return
+            }
+        }
+
+        htmlDom.style.fontSize = normalSize + '%'
+    }
+
+    setRem()
+
+    window.addEventListener('resize', setRem)
 })()
